ceb17bc9c5e2830300961b07215ce20d4696bc63
David Hybner <dah1998@yahoo.com>
2022-07-28 18:45:51 -0400

    Update Style,Index, and Js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var $StartParagraph=$('#Quiz');
+var $Intro=$('#intro'); 
+var $StartBtn= $('#StartBtn');
+
+
+$StartParagraph.text("Coding Challange Quiz");
+$Intro.text("Welcome! Try your luck at answering code-related questions within the time limit. Keep in mind with each wrong answer it will affect your overall score.")
+$StartBtn.text("Start");
+$StartParagraph.append();
+$Intro.append();
+$StartParagraph.append(); 
+
+// $StartBtn.on('click', function () {
+// console.log();
+
+// }
+
+
 
